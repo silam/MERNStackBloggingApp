3cdf8f6f4afe375f949c55f519af113597ea4fa9
Si Lam <silam@hotmail.com>
2022-11-07 09:23:41 -0600

    Recreate mongodb atlas

---
+++ backend/route/users/usersRoute.js
@@ -0,0 +1,19 @@
+const express = require("express");
+const {
+  userRegisterCtrl,
+  loginUserCtrl,
+  fetchUsersCtrl,
+  deleteUsersCtrl,
+  fetchUserDetailsCtrl,
+} = require("../../controllers/users/usersCtrl");
+const authMiddleware = require("../../middlewares/auth/authMiddleware");
+
+const userRoutes = express.Router();
+
+userRoutes.post("/register", userRegisterCtrl);
+userRoutes.post("/login", loginUserCtrl);
+userRoutes.get("/", authMiddleware, fetchUsersCtrl);
+userRoutes.delete("/:id", deleteUsersCtrl);
+userRoutes.get("/:id", fetchUserDetailsCtrl);
+
+module.exports = userRoutes;
